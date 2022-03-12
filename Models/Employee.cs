f9003b19b8208abe611a2a8711174522f6f58252
j-taylor-pixel <j42taylo@uwaterloo.ca>
2022-03-12 10:55:50 -0500

    Added more employee data
    Making a branch to save work before messing around with the migration tool

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 
 namespace Payroll_Web_App.Models
 {
+    public enum MaritialStatus
+    {
+        Single,
+        Married
+            //could add comomon in low and some other stuff,
+            //also consider making a dict
+    }
+
     public class Employee
     {
         public int Id { get; set; }//employee id
@@ -12,8 +20,15 @@ namespace Payroll_Web_App.Models
         public string LastName { get; set; }
         public string Position { get; set; }
 
-        //public string SIN { get; set; }//social insurance num, should be private
+        public string SIN { get; set; }//social insurance num, should be private
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public int MyProperty { get; set; }
 
+        //date of birth and address can OOP modeled?
+        public int hourlyRate { get; set; }
+        public int annualRate { get; set; }
+        public MaritialStatus maritialStatus { get; set; }
 
         //maybe need to add more
 
