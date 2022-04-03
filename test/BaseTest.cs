32a1c91ebefb45cecce61591fd388a21ccacc741
j-taylor-pixel <j42taylo@uwaterloo.ca>
2022-04-03 12:43:57 -0400

    added some test automation filler code

---
+++ test/BaseTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Payroll_Web_App.test
+{
+
+    //this class contains general use functionality for the test classes to share and use
+    public class BaseTest
+    {
+
+        [Fact]
+        bool run_all_tests() { return true; } //just filler code for now
+    }
+}
